7c4c2a81288d171620bf21a7eef0a3054180f788
juhriber <j.de.s.r.25@gmail.com>
2021-03-14 17:02:36 -0300

    Mudanças no arquivo Medidas

--- Program.cs
+++ Program.cs
@@ -10,17 +10,23 @@ namespace Pasta_Visual
             double cm = 100;
             double km = 1000;
 
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.Write("Digite Seu Número: ");
             metros = double.Parse(Console.ReadLine());
             Console.Write("\n");
-
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.Write("Seu Valor de Metros para Centimetros: ");
             Console.WriteLine($"{metros * cm}");
+            Console.Write("\n");
 
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
             Console.Write("Seu valor de Metros para KM: ");
             Console.WriteLine($"{metros / km}");
             Console.Write("\n");
+            Console.ResetColor();
 
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Pressione ENTER para sair!");
             Console.ReadKey();
         }
